79dc42bc012020a9a51e721eca583d9efd13f11f
Darryn Ten <darryn.ten@gmail.com>
2017-02-11 12:13:56 +0200

    PSR-2 compliant docblocks and formatting

--- src/ClarifaiModels.php
+++ src/ClarifaiModels.php
@@ -1,70 +1,79 @@
 <?php
+/**
+ * Clarifai Models
+ *
+ * @category Model
+ * @package  Clarifai
+ * @author   Darryn Ten <darrynten@github.com>
+ * @license  MIT <https://github.com/darrynten/clarifai-php/LICENSE>
+ * @link     https://github.com/darrynten/clarifai-php
+ */
 
 namespace DarrynTen\Clarifai;
 
 use DarrynTen\Clarifai\ClarifaiModel;
 
 /**
- * Single Clarifai Model
+ * Multiple Clarifai Models
  *
  * @package Clarifai
  */
-class ClarifaiModels {
-  /**
-   * A collection of models
-   *
-   * @var array $models
-   */
-  protected $models;
+class ClarifaiModels
+{
+    /**
+     * A collection of models
+     *
+     * @var array $models
+     */
+    protected $models;
 
-  /**
-   * The config
-   *
-   * @var object $config
-   */
-  protected $config;
+    /**
+     * The config
+     *
+     * @var object $config
+     */
+    protected $config;
 
-  /**
-   * The raw data
-   *
-   * @var array $rawData
-   */
-  protected $rawData;
+    /**
+     * The raw data
+     *
+     * @var array $rawData
+     */
+    protected $rawData;
 
-  /**
-   * Constructor
-   *
-   * @param array $config
-   *   The config for the model
-   * @param array $data
-   *   The data for the model
-   */
-  public function __construct(Array $config, Array $data) {
-    $this->config = $config;
-    $this->rawData = $data;
+    /**
+     * Constructor
+     *
+     * @param array $config The config for the model
+     * @param array $data   The data for the model
+     */
+    public function __construct(Array $config, Array $data)
+    {
+        $this->config = $config;
+        $this->rawData = $data;
 
-    foreach ($data as $model) {
-      $this->models[] = new ClarifaiModel($config, $model);
+        foreach ($data as $model) {
+            $this->models[] = new ClarifaiModel($config, $model);
+        }
     }
-  }
 
-  // init
-  // predict
-  // train
-  // list
-  // create
-  // get
-  // update
-  // delete
-  // search
-  //
-  // mergeConcepts
-  // deleteConcepts
-  // overwriteConcepts
-  //
-  // getVersion
-  // getVersions
-  // getOutputInfo
-  //
+    // init
+    // predict
+    // train
+    // list
+    // create
+    // get
+    // update
+    // delete
+    // search
+    //
+    // mergeConcepts
+    // deleteConcepts
+    // overwriteConcepts
+    //
+    // getVersion
+    // getVersions
+    // getOutputInfo
+    //
 }
 
