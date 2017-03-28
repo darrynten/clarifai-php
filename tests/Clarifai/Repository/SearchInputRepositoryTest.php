0650a7770b1ce5e6c0c52c8be40db27cd90db12c
dron <vojtikandrew@gmail.com>
2017-03-28 19:13:28 +0300

    space minor fix

--- tests/Clarifai/Repository/SearchInputRepositoryTest.php
+++ tests/Clarifai/Repository/SearchInputRepositoryTest.php
@@ -42,7 +42,7 @@ class SearchInputRepositoryTest extends \PHPUnit_Framework_TestCase
         $concept = $this->getFullConceptEntity('id1', true);
 
         $data['query']['ands'] = [];
-        $data['query']['ands'] = $this->getOutputConceptsQuery($data['query']['ands'] ,[$concept]);
+        $data['query']['ands'] = $this->getOutputConceptsQuery($data['query']['ands'], [$concept]);
 
         $this->request->shouldReceive('request')
             ->once()
@@ -67,7 +67,7 @@ class SearchInputRepositoryTest extends \PHPUnit_Framework_TestCase
         $concept = $this->getFullConceptEntity('id1', true);
 
         $data['query']['ands'] = [];
-        $data['query']['ands'] = $this->getInputConceptsQuery($data['query']['ands'] ,[$concept]);
+        $data['query']['ands'] = $this->getInputConceptsQuery($data['query']['ands'], [$concept]);
 
         $this->request->shouldReceive('request')
             ->once()
@@ -97,7 +97,7 @@ class SearchInputRepositoryTest extends \PHPUnit_Framework_TestCase
         $input2 = $this->getFullInputEntity()->setId('id2');
 
         $data['query']['ands'] = [];
-        $data['query']['ands'] = $this->getReverseImagesQuery($data['query']['ands'] ,[$input1]);
+        $data['query']['ands'] = $this->getReverseImagesQuery($data['query']['ands'], [$input1]);
 
         $this->request->shouldReceive('request')
             ->once()
@@ -127,7 +127,7 @@ class SearchInputRepositoryTest extends \PHPUnit_Framework_TestCase
         $input2 = $this->getFullInputEntity()->setId('id2');
 
         $data['query']['ands'] = [];
-        $data['query']['ands'] = $this->getImagesQuery($data['query']['ands'] ,[$input1]);
+        $data['query']['ands'] = $this->getImagesQuery($data['query']['ands'], [$input1]);
 
         $this->request->shouldReceive('request')
             ->once()
@@ -159,7 +159,7 @@ class SearchInputRepositoryTest extends \PHPUnit_Framework_TestCase
         $input2 = $this->getFullInputEntity()->setId('id2')->setMetaData($metadata);
 
         $data['query']['ands'] = [];
-        $data['query']['ands'] = $this->getMetadataQuery($data['query']['ands'] ,[$metadata]);
+        $data['query']['ands'] = $this->getMetadataQuery($data['query']['ands'], [$metadata]);
 
         $this->request->shouldReceive('request')
             ->once()
@@ -191,10 +191,10 @@ class SearchInputRepositoryTest extends \PHPUnit_Framework_TestCase
         $concept2 = $this->getFullConceptEntity('id2', true);
 
         $data['query']['ands'] = [];
-        $data['query']['ands'] = $this->getInputConceptsQuery($data['query']['ands'] ,[$concept1]);
-        $data['query']['ands'] = $this->getOutputConceptsQuery($data['query']['ands'] ,[$concept2]);
-        $data['query']['ands'] = $this->getMetadataQuery($data['query']['ands'] ,[$metadata]);
-        $data['query']['ands'] = $this->getImagesQuery($data['query']['ands'] ,[$input]);
+        $data['query']['ands'] = $this->getInputConceptsQuery($data['query']['ands'], [$concept1]);
+        $data['query']['ands'] = $this->getOutputConceptsQuery($data['query']['ands'], [$concept2]);
+        $data['query']['ands'] = $this->getMetadataQuery($data['query']['ands'], [$metadata]);
+        $data['query']['ands'] = $this->getImagesQuery($data['query']['ands'], [$input]);
 
         $this->request->shouldReceive('request')
             ->once()
